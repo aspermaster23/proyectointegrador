688f8fb703182cf2b388cdd1c5e1314fa9c00b77
Gonzalo Ruiz <gonzaloruiz@MBP-de-Gonzalo.home>
2021-06-20 16:16:07 -0300

    avances formulario de busqueda

--- js/master.js
+++ js/master.js
@@ -5,5 +5,27 @@
 //----------------------------GERO--------------------------------
 
 //-----------------------------GONZA----------------------------------
-let busqueda = new URLSearchParams(location.search)
-let buscar = busqueda.get('buscar');
+//resultados de busqueda
+
+//aqui llamamos a la API 
+fetch (`https://cors-anywhere.herokuapp.com/https://api.deezer.com/search?q=${buscar}`)
+//.then(objbuscado =>{
+//    return objbuscado.json()
+//})
+//.then()
+let buscar = busqueda.get('buscar'); 
+let busqueda = new URLSearchParams(location.search);
+console.log(buscar);
+
+//capturamos elemento
+let busqueda = document.querySelector('.busqueda');
+let palabraABuscar = location.search;
+
+busqueda.innerHTML = objetoBuscar.get('buscar')
+
+
+
+
+
+
+// pagina playlist
