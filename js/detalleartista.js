58f0040cd136e7e4203c8f525a2963776c6f3058
ValenFP <vflorespueyo@udesa.edu.ar>
2021-06-22 19:34:34 -0300

    Correccion de errores

--- js/detalleartista.js
+++ js/detalleartista.js
@@ -3,6 +3,24 @@ let objetoId = new URLSearchParams(location.search);
 let id =  objetoId.get('id');
 console.log(id);
 
+let listadoscanciones=document.querySelector(".listadoscanciones");
+//console.log(listadoscanciones)
+//console.log(id);
+fetch(`https://cors-anywhere.herokuapp.com/https://api.deezer.com/artist/${id}/top`)
+.then(respuesta =>{
+    return respuesta.json()
+})
+.then(dataDetallesart =>{
+    //console.log(dataDetallesart)
+
+    listadoscanciones.innerHTML += `<li><h2 class="listacanciones">Mejores 5 canciones:</h2></li>`
+
+    for(let i=0;i<5;i++){
+        listadoscanciones.innerHTML += `<li><h2>${dataDetallesart.data[i].title}</h2></li>`
+    }
+})
+
+.catch(error => console.log(error));
 let infodetallesartista=document.querySelector(".infodetallesartista");
 fetch(`https://cors-anywhere.herokuapp.com/https://api.deezer.com/artist/${id}`)
 .then(respuesta =>{
@@ -12,22 +30,6 @@ fetch(`https://cors-anywhere.herokuapp.com/https://api.deezer.com/artist/${id}`)
     console.log(dataDetallesar);
 
     infodetallesartista.innerHTML += `<h2 class="infodetalles">Artista: ${dataDetallesar.name}</h2> <article class="detallesartista"><img src=${dataDetallesar.picture_big} alt=${dataDetallesar.name} class="imgdetalles"><ul class="listadocanciones"></article>`
-})
-.catch(error => console.log(error));
-
-let listadoscanciones=document.querySelector(".listadoscanciones");
-fetch(`https://cors-anywhere.herokuapp.com/https://api.deezer.com/artist/${id}/top`)
-.then(respuesta =>{
-    return respuesta.json()
-})
-.then(dataDetallesart =>{
-    console.log(dataDetallesart);
-
-    listadoscanciones.innerHTML += `<li><h2 class="listacanciones">Mejores 5 canciones:</h2></li>`
-
-    for(let i=0;i<5;i++){
-        listadoscanciones.innerHTML += `<li><h2>${dataDetallesart.data[i].title}</h2></li>`
-    }
 })
 .catch(error => console.log(error));
 
