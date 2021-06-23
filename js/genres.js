b59225e87f974c978a386b433b2c5542c0622d70
gero-galvez <ggalvez@udesa.edu.ar>
2021-06-23 12:41:01 -0300

    genero y detalle del genero en funcionamiento

--- js/genres.js
+++ js/genres.js
@@ -1,23 +1,69 @@
 window.addEventListener ('load', function () {
+
+
+
+
+
+
+
+
+
+
+
+        let url= new URL ('https://cors-anywhere.herokuapp.com/https://api.deezer.com/genre')
+
+    let objetoId = new URLSearchParams(window.location.search);
+let id =  objetoId.get('id');
+objetoId.has('id')
+   console.log(id);
    console.log('probando link'); 
-let rap = document.querySelector('section');
 
 
 
-const apirap= "https://cors-anywhere.herokuapp.com/https://api.deezer.com/genre/116/artists";
-console.log(apirap);
 
 
+
+
+
+// const apirap= "https://cors-anywhere.herokuapp.com/https://api.deezer.com/genre/116/artists";
+// console.log(apirap);
+
+// declaro variables para las clases a las que me estare refiriendo
 let artistas=document.querySelector(".artistas");
-fetch(apirap)
+let nombreDelGenero= document.querySelector(".nombreDelGenero");
+
+// Introduzco el nombre del genero pertinente a la lista de artistas 
+fetch(`https://cors-anywhere.herokuapp.com/https://api.deezer.com/genre/${id}`)
+    .then(respuesta=>{
+        return respuesta.json()
+    })
+    .then(dataNombreGenero=>{
+        console.log(dataNombreGenero)
+        nombreDelGenero.innerHTML=` Artistas de: ${dataNombreGenero.name}`
+    })
+
+
+
+
+
+
+// 
+fetch(`https://cors-anywhere.herokuapp.com/https://api.deezer.com/genre/${id}/artists`)
+
 .then(respuesta =>{
     return respuesta.json()
 })
 .then(dataRap =>{
-    //console.log(dataRap);
-    for(let i =0;i<20;i++){
-        artistas.innerHTML += `<article class="artista"><a href="detalleartista.html?id=${dataRap.data[i].id}"> <img src=${dataRap.data[i].picture_big}> </a> <h2>${dataRap.data[i].name}</h2> </article>`
-    }    
+    console.log(dataRap)
+    for(let i =0;i<24;i++){
+    
+        artistas.innerHTML += `<article class="artista">
+        <a href="detalleartista.html?id=${dataRap.data[i].id}">
+         <img src=${dataRap.data[i].picture_big}>
+          </a>
+          <h3>${dataRap.data[i].name}</h3>
+           </article>`
+       }    
 })
 .catch(error=>console.log(error))
 
@@ -36,94 +82,71 @@ fetch(apirap)
 
 
 
+   // let rap = document.querySelector('section');
 
 
 
+// const apirap= "https://cors-anywhere.herokuapp.com/https://api.deezer.com/genre/116/artists";
+// console.log(apirap);
 
 
-
-
-
-
-
-
-// // trayendo el genero de rap opcion 2
+// let artistas=document.querySelector(".artistas");
 // fetch(apirap)
-//     .then(response =>{
-//         return response.json()
-//     })
-//     .then(dataRap=> imprimirHTML(dataRap.data))
-//     .catch(error=>console.log(error))
-
-//     function imprimirHTML(dataRap){
-//         dataRap.forEach(usuario=>{  
-//             console.log(usuario);
-//             const li = document.createElement('li');
-//             const{name, picture_small, id } =
-//             usuario;
-
-//             li.innerHTML = `<article class="discoalbum">
-//             <a href="detalleartista.html?id=${id}">
-//              <img src=${picture_small}> </a> 
-//              <h2>${name}</h2> 
-//              </article>
-//             `;
-//             document.querySelector('section').appendChild(li);
-        
-//         });
-//     }
+// .then(respuesta =>{
+//     return respuesta.json()
+// })
+// .then(dataRap =>{
+//     //console.log(dataRap);
+//     for(let i =0;i<24;i++){
+//         artistas.innerHTML += `<article class="artista"><a href="detalleartista.html?id=${dataRap.data[i].id}"> <img src=${dataRap.data[i].picture_big}> </a> <h2>${dataRap.data[i].name}</h2> </article>`
+//     }    
+// })
+// .catch(error=>console.log(error))
 
 
-// // trayendo el genero de rap opcion 2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let rap = document.querySelector('section');
+
+
+
+// const apirap= "https://cors-anywhere.herokuapp.com/https://api.deezer.com/genre/116/artists";
+// console.log(apirap);
+
+
+// let artistas=document.querySelector(".artistas");
 // fetch(apirap)
-//     .then(response =>{
-//         return response.json()
-//     })
-//     .then(dataRap=> imprimirHTML(dataRap.data))
-//     .catch(error=>console.log(error))
-
-//     function imprimirHTML(dataRap){
-//         dataRap.forEach(usuario=>{  
-//             console.log(usuario);
-//             const li = document.createElement('li');
-//             const{name, picture_small, id } =
-//             usuario;
-
-//             li.innerHTML = `
-//             <article class="discoalbum">
-//             <a href="detalleartista.html?id=${id}" target="_blank">
-//              Artista: ${name}
-//               <img src= "${picture_small}">
-//              </a>
-//              </article>
-//             `;
-//             document.querySelector('section').appendChild(li);
-        
-//         });
-//     }
-
-
-// // trayendo el genero de rap opcion 1 
-// fetch(apirap)
-//     .then(response =>{
-//         return response.json()
-//     })
-//     .then(dataRap=>{
-//     console.log(dataRap);
-//     // let artistasDeRap= [ dataRap.data];
-//     // console.log(artistasDeRap);
-//     for(let i =0;i<10;i++){     
-//     rap.innerHTML+= ` <ul class="listas">
-//                         <li  class="canciones-en-lista" > <a href="detalleartista.html?id=${dataRap.data[i].id}" target="_blank"> -${dataRap.data[i].name} <></img src: ${dataRap.data[i].picture_small}a> </li > 
-//                     </ul> </a>`
-//     }
-//     })
-//     .catch(error=>console.log(error))
+// .then(respuesta =>{
+//     return respuesta.json()
+// })
+// .then(dataRap =>{
+//     //console.log(dataRap);
+//     for(let i =0;i<24;i++){
+//         artistas.innerHTML += `<article class="artista"><a href="detalleartista.html?id=${dataRap.data[i].id}"> <img src=${dataRap.data[i].picture_big}> </a> <h2>${dataRap.data[i].name}</h2> </article>`
+//     }    
+// })
+// .catch(error=>console.log(error))
 
 
 
-// rock.addEventListener('click', function () {innerHTML= apirap}
-
-//     this.innerHTML=data.results
 })
 
