275127927c05bff07845690e2e73b8b3053e94aa
Gonzalo Ruiz <gonzaloruiz@MacBook-Pro-de-Gonzalo.local>
2021-06-25 19:35:50 -0300

    codigo mejorado

--- js/buscar.js
+++ js/buscar.js
@@ -1,21 +1,14 @@
-let busqueda = new URLSearchParams(location.search) //location.search  me trae la info de la query string - 
-                                                   //url search params la pasa de formato string a objeto
-
-let buscar = busqueda.get('buscar') // enlazo la propiedad declarada anteriormente con un .get (enlaza esta propiedad con una función 
-                                    //que será llamada cuando la propiedad es buscada.)
-let resultados = document.querySelector('.resultados') //caputuro la clase de una seccion de resultadosbusqueda.html
-let contenido = ''                                      // declaro una variable vacia para despues agregarle lo que serian los resultados de la busqueda
-let titulo = document.querySelector('.titulogonchy')   // declaro una variable al titulo de resultadosbusqueda.html
-
-titulo.innerHTML += buscar              // llamo a la variable que declaramos en la linea anterior (la cual contiene a la clase '.titulogonchy' y
-                                        // con la propiedad '.innerhtml' le modifico su estructura 
-   //el += me permite que no se me borre el titulo y que lo que escribio el usuario en el campo "buscar" se agregue al titulo
-
+let busqueda = new URLSearchParams(location.search)   
+let buscar = busqueda.get('buscar') 
+let resultados = document.querySelector('.resultados') 
+let contenido = ''                                      
+let titulo = document.querySelector('.titulogonchy')  
+titulo.innerHTML += buscar         
 fetch(`https://cors-anywhere.herokuapp.com/https://api.deezer.com/search?q=${buscar}`)
 .then(respuesta =>{
    return respuesta.json()       // en esta promesa transformo los datos en objeto literal con el .json
 })
-.then(dataBusqueda =>{ // llamo al array y le indico que quiero que me traiga todo lo que contenga el array y que me lo traiga de uno en uno
+.then(dataBusqueda =>{     // llamo al array y le indico que quiero que me traiga todo lo que contenga el array y que me lo traiga de uno en uno
    for(let i=0; i < dataBusqueda.data.length; i++){      
 
       //uso la variable anteriormente declarada como contenido para que contenga los datos que quiero que me traiga el array
@@ -27,7 +20,6 @@ fetch(`https://cors-anywhere.herokuapp.com/https://api.deezer.com/search?q=${bus
    if(dataBusqueda.data.length == 0){
       alert('La busqueda no trajo resultados')
    }
-   
 })
 .catch(error =>{
    console.log(error);
--- js/playlist.js
+++ js/playlist.js
@@ -1,25 +1,20 @@
-let cancionesFavoritas = localStorage.getItem('pepito') // Traigo los datos del localStorage con getItem que estan dentro de favoritos
+let cancionesFavoritas = localStorage.getItem('pepito') // Traigo los datos del localStorage con getItem 
 let arrayplaylist = JSON.parse(cancionesFavoritas)  // Los transformo en un array // Parsee cancionesFavoritas
-
-let imgplaylist = document.querySelector('.imgplaylist')    // Declare la variables infoplaylist para capturar el elemento en donde quiero que se muestren las fotos 
+let imgplaylist = document.querySelector('.imgplaylist')    // Declare la variables imgplaylist para capturar el elemento en donde quiero que se muestren las fotos 
 let tituloplaylist = document.querySelector('.titulogonchy')
-
-
-if(arrayplaylist.length == 0){ // Lo que le indique a este bloque fue basicamente que si el array cancionesPlaylist esta vacio, que diga:
+if(arrayplaylist.length == 0){ // Lo que le indique a este bloque fue basicamente que si el array cancionesFavoritas esta vacio, que diga:
     tituloplaylist.innerHTML = 'No agregaste ninguna cancion a tu playlist'
 } else{ // Hice fetch para que muestre lo que el usuario puso en la playlist
     for(let i=0 ; i < arrayplaylist.length; i++){
         fetch(`https://cors-anywhere.herokuapp.com/https://api.deezer.com/track/${arrayplaylist[i]}`)        
-                                                                            // Interpolo el array carrayPlaylist, que es en donde esta la informacion. 
+                                                                    // Interpolo el arrayPlaylist, que es en donde esta la informacion. 
         .then(function(respuesta){
             return respuesta.json()
         })
         .then(function(track){
-         console.log(track);
          imgplaylist.innerHTML += `<a href="detallecancion.html?id=${track.id}"><img  class="links" src="${track.album.cover_xl}" alt="Album${track.album.title}">${track.title}</img></a>`
 
         })
-
         .catch(function(error){
             console.log(error);
         })}}        
